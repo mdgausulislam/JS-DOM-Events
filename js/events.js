98eea0476f69ed1c3b8fd4cf79117312c9ef6025
unknown <gausulislam5@gmail.com>
2023-09-17 00:55:40 +0600

    event-listener

--- js/events.js
+++ js/events.js
@@ -10,3 +10,31 @@ function makeBlue() {
     document.body.style.backgroundColor = 'blue';
 }
 console.log(makeBlueButton);
+
+const purpleButton = document.getElementById('make-purple');
+purpleButton.onclick = function makePurple() {
+    document.body.style.backgroundColor = 'purple';
+
+}
+
+const pinkButton = document.getElementById('make-pink');
+
+pinkButton.addEventListener('click', myPink);
+
+
+function myPink() {
+    document.body.style.backgroundColor = 'pink';
+
+}
+
+const greenButton=document.getElementById('make-green');
+    
+greenButton.addEventListener('click',  function() {
+ document.body.style.backgroundColor='green';
+ 
+});
+
+ // final code ata use korbo besi
+ document.getElementById('make-orange').addEventListener('click',function(){
+    document.body.style.backgroundColor='orange';
+})
