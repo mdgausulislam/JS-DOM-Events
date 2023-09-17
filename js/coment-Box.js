2ab74ff31558c84f92ddd0919d2ba01863d52890
unknown <gausulislam5@gmail.com>
2023-09-17 09:00:22 +0600

    update eventlistener

--- js/coment-Box.js
+++ js/coment-Box.js
@@ -1,12 +1,12 @@
 document.getElementById('btn-post').addEventListener('click',function(){
     const commentBox=document.getElementById('new-Comment');
     console.log(commentBox);
-    const newComent=commentBox.value;
+    const newComment=commentBox.value;
   
 
     const commentContainer=document.getElementById('coment-container');
     const p=document.createElement('p');
-    p.innerText=newComent;
+    p.innerText=newComment;
     commentContainer.appendChild(p);
 
     commentBox.value='';
